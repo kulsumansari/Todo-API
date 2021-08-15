193ba7a0905fedb06a93d5ea1f8f008487afb571
kulsumansari <kulsumansari4@gmail.com>
2021-08-15 17:16:57 +0530

    update

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const cors =require('cors');
-
+const fs =require('fs');
 const app =express();
 
 const taskRouter =require('./routes/taskRouter')
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
+
+let text= `<embed type="text/markdown" src="https://saima422.github.io/mardown-trial/" height="100%" width="100%"/>`;
+fs.writeFileSync("./public/index.html", text);
 
 app.use('/tasks',taskRouter)
 
